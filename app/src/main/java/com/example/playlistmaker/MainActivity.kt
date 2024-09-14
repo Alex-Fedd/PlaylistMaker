2dd26a9878884d512fb9df075c59c663cb1f3490
Alex-Fedd <blackyblackmail@yandex.ru>
2024-09-14 15:37:35 +0300

    Commit 2 - спринт 8, 2-я дз из трех.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.mediateka_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        mediaButton.setOnClickListener{
+            Toast.makeText(this, "Медиааа!", Toast.LENGTH_LONG).show()
+        }
+
+        settingsButton.setOnClickListener{
+            Toast.makeText(this,"Настрой-ка мне!", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+    //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
     }
 }
 
